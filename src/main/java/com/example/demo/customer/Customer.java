227d46ad6e9d3171540df672d25e6bc1366fd02b
TUnit17 <thuang177@gmail.com>
2021-10-08 11:19:23 -0700

    Jackson Library intro with @JsonIgnore and @JsonProperty

--- src/main/java/com/example/demo/customer/Customer.java
+++ src/main/java/com/example/demo/customer/Customer.java
@@ -1,14 +1,26 @@
 package com.example.demo.customer;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 class Customer{
     private final Long id;
     private final String name;
+    private final String password;
 
-    public Customer(Long id, String name) {
+
+    public Customer(Long id, String name, String password) {
         this.id = id;
         this.name = name;
+        this.password = password;
     }
 
+    @JsonIgnore // will not display password in the http response
+    public String getPassword() {
+        return password;
+    }
+
+    @JsonProperty("my_customer_fieldname_fromJsonProperty")
     public Long getId() {
         return id;
     }
@@ -17,11 +29,14 @@ class Customer{
         return name;
     }
 
+
+
     @Override
     public String toString() {
         return "Customer{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
                 '}';
     }
 }
--- src/main/java/com/example/demo/customer/CustomerFakeRepository.java
+++ src/main/java/com/example/demo/customer/CustomerFakeRepository.java
@@ -1,8 +1,5 @@
 package com.example.demo.customer;
 
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,9 +10,9 @@ public class CustomerFakeRepository implements CustomerRepo{
     @Override
     public List<Customer> getCustomers(){
         return Arrays.asList(
-                new Customer(1L, "James Bond"),
-                new Customer(2L, "Tony Stark"),
-                new Customer(3L, "Peter Parker")
+                new Customer(1L, "James Bond", "password123"),
+                new Customer(2L, "Tony Stark", "password456"),
+                new Customer(3L, "Peter Parker", "password789")
         );
     }
 }
