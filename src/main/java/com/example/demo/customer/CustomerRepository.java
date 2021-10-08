e9acc04a5a4a95964052bc594941317948c0b97d
TUnit17 <thuang177@gmail.com>
2021-10-07 22:19:10 -0700

    @Qualified(fake)

--- src/main/java/com/example/demo/customer/CustomerRepository.java
+++ src/main/java/com/example/demo/customer/CustomerRepository.java
@@ -1,11 +1,13 @@
 package com.example.demo.customer;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 import java.util.List;
 
-@Component
+@Component // makes CustomerService a bean that we can inject in multiple places,makes CustomerService a singleton that if used in multiple places it is all the same
+@Primary // this is my primary implementation
 public class CustomerRepository implements CustomerRepo{
     @Override
     public List<Customer> getCustomers(){
--- src/main/java/com/example/demo/customer/CustomerService.java
+++ src/main/java/com/example/demo/customer/CustomerService.java
@@ -1,18 +1,18 @@
 package com.example.demo.customer;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 
-@Component
-// makes CustomerService a bean that we can inject in multiple places
-// makes CustomerService a singleton that if used in multiple places it is all the same
+@Component // makes CustomerService a bean that we can inject in multiple places,makes CustomerService a singleton that if used in multiple places it is all the same
 public class CustomerService {
 
     private final CustomerRepo customerRepo;
 
-    public CustomerService(CustomerRepo customerRepo) {
+    // inject the CustomerFakeRepository here
+    public CustomerService(@Qualifier("fake") CustomerRepo customerRepo) {
         this.customerRepo = customerRepo;
     }
 
