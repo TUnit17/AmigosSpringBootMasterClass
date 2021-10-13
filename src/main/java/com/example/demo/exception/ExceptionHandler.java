8903cdb7068dba02178ccf30aa6b0d33b3b0a845
TUnit17 <thuang177@gmail.com>
2021-10-12 20:44:48 -0700

    more complex exception handling

---
+++ src/main/java/com/example/demo/exception/ExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.example.demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.ZonedDateTime;
+
+// This class is confusing
+// Why are there so many layers of exceptions?
+// to make it more generic?
+public class ExceptionHandler {
+    public ResponseEntity<Object> handleApiRequestException(ApiRequestException e){
+        ApiException apiException = new ApiException(e.getMessage(),
+                e,
+                HttpStatus.BAD_REQUEST,
+                ZonedDateTime.now());
+        return new ResponseEntity<>(apiException,
+                HttpStatus.BAD_REQUEST);
+    }
+}
