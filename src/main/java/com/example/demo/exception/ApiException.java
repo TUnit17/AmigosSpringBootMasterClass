bb6c2c463a87003143adbb69f0de46f5a3372f4a
TUnit17 <thuang177@gmail.com>
2021-10-17 21:10:07 -0700

    convert lombok @Data Class => record

--- src/main/java/com/example/demo/exception/ApiException.java
+++ src/main/java/com/example/demo/exception/ApiException.java
@@ -1,50 +1,16 @@
 package com.example.demo.exception;
 
-import lombok.Data;
 import org.springframework.http.HttpStatus;
 
 import java.time.ZonedDateTime;
 
-@Data // this creates athe constructor, getters, toString
-public class ApiException {
-    private final String message;
-    private final Throwable throwable;
-    private final HttpStatus httpStatus;
-    private final ZonedDateTime zonedDateTime;
+// use record which is a:
+// private final field + constructor + getter + setter + toString + equals hashcode
+public record ApiException(
+        String message,
+        Throwable throwable,
+        HttpStatus httpStatus,
+        ZonedDateTime zonedDateTime
+) {
 
-//    public ApiException(String message,
-//                        Throwable throwable,
-//                        HttpStatus httpStatus,
-//                        ZonedDateTime zonedDateTime) {
-//        this.message = message;
-//        this.throwable = throwable;
-//        this.httpStatus = httpStatus;
-//        this.zonedDateTime = zonedDateTime;
-//    }
-//
-//    public String getMessage() {
-//        return message;
-//    }
-//
-//    public Throwable getThrowable() {
-//        return throwable;
-//    }
-//
-//    public HttpStatus getHttpStatus() {
-//        return httpStatus;
-//    }
-//
-//    public ZonedDateTime getZonedDateTime() {
-//        return zonedDateTime;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "ApiException{" +
-//                "message='" + message + '\'' +
-//                ", throwable=" + throwable +
-//                ", httpStatus=" + httpStatus +
-//                ", zonedDateTime=" + zonedDateTime +
-//                '}';
-//    }
 }
